04d1d64ee0a034c8ab3e8ddeb631e6e9ebdb27fb
Akshay <patilakshay66327@gmail.com>
2019-08-19 01:27:44 +0530

    first commit

---
+++ MainClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Demo
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            DatabaseHelper dbHelper = new DatabaseHelper();
+            do
+            {
+
+                Console.WriteLine("Enter the operation do you want to perform on database like display, insert , update, delete");
+                string ch = Console.ReadLine();
+                switch (ch)
+                {
+                    case "display":
+                        dbHelper.createConnection();
+                        break;
+
+                    case "insert":
+                        dbHelper.insertIntoDatabase();
+                        dbHelper.createConnection();
+                        break;
+
+                    case "update":
+                        dbHelper.updateIntoDatabase();
+                        dbHelper.createConnection();
+                        break;
+
+                    case "delete":
+                        dbHelper.deleteRecord();
+                        dbHelper.createConnection();
+                        break;
+
+                    default:
+                        break;
+                } 
+            } while (true);
+        }
+    }
+}
---
+++ databaseHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Demo
+{
+    class DatabaseHelper
+    {
+        public void createConnection()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\OWNER\\Documents\\Fruits.accdb;Persist Security Info = False;");
+            OleDbCommand oleDbCommand = new OleDbCommand("select * from fruitDetails", oleDbConnection);
+            oleDbConnection.Open();
+            var reader = oleDbCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["Name"]+"\t\t\t "+reader["Quantity"] + " \t\t\t" + reader["price"]);
+            }
+            oleDbConnection.Close();
+            Console.ReadLine();
+        }
+
+        public void insertIntoDatabase()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\OWNER\\Documents\\Fruits.accdb;Persist Security Info = False;");
+            OleDbCommand oleDbCommand = new OleDbCommand("insert into fruitDetails values (12,'Royal lichi',20,45000)",oleDbConnection);
+            oleDbConnection.Open();
+            var reader = oleDbCommand.ExecuteNonQuery();
+            Console.WriteLine(reader+"row is effected");
+            oleDbConnection.Close();
+            Console.ReadLine();
+        }
+
+        public void updateIntoDatabase()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\OWNER\\Documents\\Fruits.accdb;Persist Security Info = False;");
+            OleDbCommand oleDbCommand = new OleDbCommand("update fruitDetails set Name = \"strawberry\" where ID = 7", oleDbConnection);
+            oleDbConnection.Open();
+            var reader = oleDbCommand.ExecuteNonQuery();
+            Console.WriteLine(reader+" row Get Effected");
+            oleDbConnection.Close();
+            Console.ReadLine();
+        }
+
+        public void deleteRecord()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\OWNER\\Documents\\Fruits.accdb;Persist Security Info = False;");
+            OleDbCommand oleDbCommand = new OleDbCommand("delete from fruitDetails where ID = 12", oleDbConnection);
+            oleDbConnection.Open();
+            var reader = oleDbCommand.ExecuteNonQuery();
+            Console.WriteLine(reader + " row Get Effected");
+            oleDbConnection.Close();
+            Console.ReadLine();
+        }
+    }
+}
